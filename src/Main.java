d646469e7f99ad1d66f9af885e3d8dd1997b2ebd
ritikamuruganandam06 <ritikamuruganandam@gmail.com>
2026-08-17 22:25:56 +0530

    added display and remove slots logic

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Vehicle vehicle = new Vehicle("TN38AB1234",VehicleType.CAR);
@@ -10,6 +11,7 @@ public class Main {
         // System.out.println("Parked vehicle: "+ slot.getVehicle().getVehicleNumber());
         // slot.removeVehicle();
         // System.out.println("Slot available: " + slot.isAvailable());
+        Scanner scanner = new Scanner(System.in);
         ParkingLot parkingLot = new ParkingLot();
         ParkingSlot bikeSlot1 = new ParkingSlot("B1", VehicleType.BIKE);
         ParkingSlot bikeSlot2 = new ParkingSlot("B2", VehicleType.BIKE);
@@ -22,26 +24,140 @@ public class Main {
         parkingLot.addSlot(carSlot2);
         parkingLot.addSlot(truckSlot);
         System.out.println("Parking lot created !");
-        Vehicle car = new Vehicle("TN38AB1234",VehicleType.CAR);
-        ParkingSlot parkedSlot = parkingLot.parkVehicle(car);
-        if(parkedSlot != null) {
-            System.out.println("Vehicle parked successfully");
-            System.out.println("vehicle number : " + car.getVehicleNumber());
-            System.out.println("Slot: "+parkedSlot.getSlotId());
-        }
-        else {
-            System.out.println("No suitable parking slot available");
-        }
-        parkingLot.displayStatus();
-        boolean removed =
-        parkingLot.removeVehicle("TN38AB1234");
+        // Vehicle car = new Vehicle("TN38AB1234",VehicleType.CAR);
+        // ParkingSlot parkedSlot = parkingLot.parkVehicle(car);
+        // if(parkedSlot != null) {
+        //     System.out.println("Vehicle parked successfully");
+        //     System.out.println("vehicle number : " + car.getVehicleNumber());
+        //     System.out.println("Slot: "+parkedSlot.getSlotId());
+        // }
+        // else {
+        //     System.out.println("No suitable parking slot available");
+        // }
+        // parkingLot.displayStatus();
+        // boolean removed =
+        // parkingLot.removeVehicle("TN38AB1234");
 
-        if (removed) {
-            System.out.println("Vehicle removed successfully!");
-        } else {
-            System.out.println("Vehicle not found!");
-        }
+        // if (removed) {
+        //     System.out.println("Vehicle removed successfully!");
+        // } else {
+        //     System.out.println("Vehicle not found!");
+        // }
 
-        parkingLot.displayStatus();
+        // parkingLot.displayStatus();
+        while(true) {
+            System.out.println("\n========== PARKING SYSTEM ==========");
+
+            System.out.println("1. Park Vehicle");
+            System.out.println("2. Remove Vehicle");
+            System.out.println("3. Display Status");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter choice: ");
+
+            int choice = scanner.nextInt();
+            switch(choice) {
+                case 1 : 
+                     System.out.print(
+                            "Enter vehicle number: "
+                    );
+
+                    String vehicleNumber =
+                            scanner.next();
+
+                    System.out.print(
+                            "Enter vehicle type (BIKE/CAR/TRUCK): "
+                    );
+
+                    String typeInput =
+                            scanner.next();
+
+                    VehicleType type =
+                            VehicleType.valueOf(
+                                    typeInput.toUpperCase()
+                            );
+
+                    Vehicle vehicle =
+                            new Vehicle(
+                                    vehicleNumber,
+                                    type
+                            );
+
+                    ParkingSlot parkedSlot =
+                            parkingLot.parkVehicle(vehicle);
+
+                    if (parkedSlot != null) {
+
+                        System.out.println(
+                                "Vehicle parked successfully!"
+                        );
+
+                        System.out.println(
+                                "Slot: "
+                                + parkedSlot.getSlotId()
+                        );
+
+                    } else {
+
+                        System.out.println(
+                                "No suitable slot available."
+                        );
+                    }
+
+                    break;
+                case 2 :
+
+                    System.out.print(
+                            "Enter vehicle number: "
+                    );
+
+                    String number =
+                            scanner.next();
+
+                    boolean removed =
+                            parkingLot.removeVehicle(number);
+
+                    if (removed) {
+
+                        System.out.println(
+                                "Vehicle removed successfully!"
+                        );
+
+                    } else {
+
+                        System.out.println(
+                                "Vehicle not found!"
+                        );
+                    }
+
+                    break;
+
+
+                case 3:
+
+                    parkingLot.displayStatus();
+
+                    break;
+
+
+                case 4:
+
+                    System.out.println(
+                            "Thank you for using the parking system!"
+                    );
+
+                    scanner.close();
+
+                    return;
+
+
+                default:
+
+                    System.out.println(
+                            "Invalid choice!"
+                    );
+
+            }
+        }
     }
 }
